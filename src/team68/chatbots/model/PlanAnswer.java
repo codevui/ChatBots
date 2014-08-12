59575aa401edbc2a74dffda3c3b3a17f1ffedb1c
khanhoatink4 <khanhoait.bka@gmail.com>
2014-08-12 11:28:10 +0700

    up plananswer

---
+++ src/team68/chatbots/model/PlanAnswer.java
@@ -0,0 +1,40 @@
+package team68.chatbots.model;
+
+public class PlanAnswer {
+	private int id;
+	private int id_question;
+	private String content;
+	private boolean isTrue;
+	public PlanAnswer(){
+		this.id = 0;
+		this.id_question = 0;
+		this.content = "";
+		this.isTrue = false;
+	}
+	public void setId(int id){
+		this.id = id;
+	}
+	public int getId(){
+		return this.id;
+	}
+	public void setIdQuestion(int idq){
+		this.id_question = idq;
+	}
+	public int getIdQuestion(){
+		return this.id_question;
+	}
+	public void setContent(String content){
+		this.content = content;
+	}
+	public String getContent(){
+		return this.content;
+	}
+	public void setIsTrue(boolean is){
+		this.isTrue = is;
+	}
+	public boolean getIsTrue(){
+		return this.isTrue;
+	}
+	
+
+}
