894d286dc83023217b27d9e0dbc5e3ada10670ad
igisik <nvhoang192@gmail.com>
2014-08-15 00:00:44 +0700

    Nothing
    sss

--- src/team68/chatbots/model/entity/PlanAnswer.java
+++ src/team68/chatbots/model/entity/PlanAnswer.java
@@ -1,45 +1,3 @@
-<<<<<<< HEAD:src/team68/chatbots/model/PlanAnswer.java
-package team68.chatbots.model;
-
-public class PlanAnswer {
-	private int id;
-	private int id_question;
-	private String content;
-	private boolean isTrue;
-	public PlanAnswer(){
-		this.id = 0;
-		this.id_question = 0;
-		this.content = "";
-		this.isTrue = false;
-	}
-	public void setId(int id){
-		this.id = id;
-	}
-	public int getId(){
-		return this.id;
-	}
-	public void setIdQuestion(int idq){
-		this.id_question = idq;
-	}
-	public int getIdQuestion(){
-		return this.id_question;
-	}
-	public void setContent(String content){
-		this.content = content;
-	}
-	public String getContent(){
-		return this.content;
-	}
-	public void setIsTrue(boolean is){
-		this.isTrue = is;
-	}
-	public boolean getIsTrue(){
-		return this.isTrue;
-	}
-	
-
-}
-=======
 package team68.chatbots.model.entity;
 
 public class PlanAnswer {
@@ -80,4 +38,4 @@ public class PlanAnswer {
 	
 
 }
->>>>>>> origin/master:src/team68/chatbots/model/entity/PlanAnswer.java
+
